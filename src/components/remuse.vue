dc0a8e49ad2de66ef2012a46befe68b5fb9a3534
shanshan <347996377@qq.com>
2017-12-22 17:09:41 +0800

    resume

---
+++ src/components/remuse.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="remuse styleEditor">
+    <div class='code' v-html='remuseStyle'></div>
+    <pre v-html="remuse"></pre>
+  </div>
+</template>
+
+<script>
+import Prism from 'prismjs'
+export default {
+  props:['code'],
+  computed:{
+    remuseStyle:function(){
+     return `<style>${this.code}</style>`
+   },
+   remuse:function(){
+    return Prism.highlight(this.code, Prism.languages.css)
+
+   }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
